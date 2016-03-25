4f70d791d4293fa5608aa965f2b054c4806b39ea
Plastikaweb <info@plastikaweb.com>
2016-03-25 11:05:00 +0100

    builder patean

--- section2_creational_patterns/js/script.js
+++ section2_creational_patterns/js/script.js
@@ -1,26 +1,48 @@
 (function (win, $) {
-    function RedCircle() {
-
-    }
-    RedCircle.prototype.create = function () {
+    //CIRCLE
+    function Circle() {
         this.item = $('<div class="circle"></div>');
-        return this;
-    };
-    function BlueCircle() {
-
     }
-    BlueCircle.prototype.create = function () {
-        this.item = $('<div class="circle" style="background: blue;"></div>');
-        return this;
+    Circle.prototype.move = function(left, top) {
+        this.item.css('left', left);
+        this.item.css('top', top);
+    };
+    Circle.prototype.color = function(color) {
+      this.item.css('background', color);
+    };
+    Circle.prototype.get = function() {
+        return this.item;
+    };
+    //RED CIRCLE
+    function RedCircleBuilder() {
+        this.item = new Circle();
+        this.init();
+    }
+    RedCircleBuilder.prototype.init = function () {
+
+    };
+    RedCircleBuilder.prototype.get = function() {
+       return this.item;
+    };
+    //BLUE CIRCLE
+    function BlueCircleBuilder() {
+        this.item = new Circle();
+        this.init();
+    }
+    BlueCircleBuilder.prototype.init = function () {
+        this.item.color('blue');
+    };
+    BlueCircleBuilder.prototype.get = function() {
+        return this.item;
     };
 
     var CircleFactory = function () {
           this.types = {};
           this.create = function (type) {
-              return new this.types[type]().create();
+              return new this.types[type]().get();
           };
           this.register = function(type, cls) {
-              if(cls.prototype.create) {
+              if(cls.prototype.init && cls.prototype.get) {
                   this.types[type] = cls;
               }
           };
@@ -33,22 +55,21 @@
               var _aCircle = [],
                 _stage = $('.advert'),
                 _cf = new CircleFactory();
-              _cf.register('red', RedCircle);
-              _cf.register('blue', BlueCircle);
+              _cf.register('red', RedCircleBuilder);
+              _cf.register('blue', BlueCircleBuilder);
 
               function _position(circle, left, top) {
-                  circle.css('left', left);
-                  circle.css('top', top);
+                  circle.move(left, top);
               }
 
               function create(left, top, type) {
-                  var circle = _cf.create(type).item;
-                  _position(circle, left, top);
+                  var circle = _cf.create(type);
+                  circle.move(left, top);
                   return circle;
               }
 
               function add(circle) {
-                  _stage.append(circle);
+                  _stage.append(circle.get());
                   _aCircle.push(circle);
               }
 
